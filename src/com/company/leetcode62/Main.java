35e647d32705cfb81abd34f8f2c2e9f7a788b6af
yongjianmeng <yongjianmeng@gmail.com>
2018-09-19 21:28:04 +0800

    completed 62

---
+++ src/com/company/leetcode62/Main.java
@@ -0,0 +1,13 @@
+package com.company.leetcode62;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.uniquePaths(0, 0));
+        System.out.println(solution.uniquePaths(1, 6));
+        System.out.println(solution.uniquePaths(6, 1));
+        System.out.println(solution.uniquePaths(3, 2));
+        System.out.println(solution.uniquePaths(7, 3));
+    }
+}
---
+++ src/com/company/leetcode62/Solution.java
@@ -0,0 +1,28 @@
+package com.company.leetcode62;
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+
+        if (n == 1 || m == 1) {
+            return 1;
+        }
+
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+}
