2cd3de36a03fb7fe6da00d32518bc7e67fb1a61f
yongjianmeng <yongjianmeng@gmail.com>
2018-09-20 21:45:43 +0800

    completed 91

---
+++ src/com/company/leetcode91/Main.java
@@ -0,0 +1,12 @@
+package com.company.leetcode91;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+//        System.out.println(solution.numDecodings("12"));
+//        System.out.println(solution.numDecodings("226"));
+//        System.out.println(solution.numDecodings("12382317"));
+        System.out.println(solution.numDecodings("923112342382317126674542"));
+    }
+}
