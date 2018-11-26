82708fbaa8f6d1c674690653b0a7bd0426ede5a7
yongjianmeng <yongjianmeng@gmail.com>
2018-11-26 20:14:00 +0800

    completed 100.

---
+++ src/com/company/leetcode100/Solution.java
@@ -0,0 +1,18 @@
+package com.company.leetcode100;
+
+import com.company.utils.TreeNode;
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
