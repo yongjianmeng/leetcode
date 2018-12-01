4772d22a2d40da5ae5607049760c1ddfa42f72f6
yongjianmeng <yongjianmeng@gmail.com>
2018-12-01 20:20:09 +0800

    completed 700.

---
+++ src/com/company/leetcode700/Solution.java
@@ -0,0 +1,18 @@
+package com.company.leetcode700;
+
+import com.company.utils.TreeNode;
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
